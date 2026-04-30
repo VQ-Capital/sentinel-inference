a8bdb32d555fbab90e1f884d7ac66dee4b7b704a
Azmi Sahin <6445951+azmisahin@users.noreply.github.com>
2026-04-30 21:05:51 +0300

    fix: update DNA weights for improved accuracy in machine learning simulation

--- src/weights.rs
+++ src/weights.rs
@@ -3,18 +3,18 @@
 pub fn get_dna_weights() -> Vec<f32> {
     let weights_data = vec![
         //  HOLD,    BUY,    SELL
-        0.2014, 0.3419, -0.3875, // F0: Price Velocity (Z-Score)
-        -0.0571, 0.6251, -0.4000, // F1: Orderbook Imbalance
-        -0.2874, -0.1460, -0.4871, // F2: Neural Sentiment
-        -0.3496, -0.3786, -0.2330, // F3: Chain Urgency
-        -0.2739, -0.3713, -0.0890, // F4: RSI
-        0.2163, -0.1000, -0.2741, // F5: Volatility
-        0.0000, 0.2000, -0.2566, // F6: Taker Ratio
-        -0.1753, 0.4122, -0.3247, // F7: Intensity (Tick count)
-        0.2864, -0.3848, 0.4565, // F8: Position in Range
-        0.1565, 0.2834, -0.0986, // F9: Orderbook Depth
-        -0.0532, 0.4870, 0.0270, // F10: Time Sine (Intraday)
-        0.0000, -0.1838, -0.0717, // F11: Last Close Price
+         0.2194,  0.0126,  -0.3038, // F0: Price Velocity (Z-Score)
+         0.0568,  -0.2281,  -0.0222, // F1: Orderbook Imbalance
+         -0.0645,  -0.2583,  0.0000, // F2: Neural Sentiment
+         0.0000,  -0.2321,  0.0652, // F3: Chain Urgency
+         -0.2541,  0.3632,  0.2662, // F4: RSI
+         0.0000,  0.1778,  -0.4357, // F5: Volatility
+         0.0055,  0.1160,  0.0000, // F6: Taker Ratio
+         -0.0909,  0.0000,  0.0000, // F7: Intensity (Tick count)
+         -0.2875,  -0.1922,  -0.0842, // F8: Position in Range
+         0.0000,  0.0000,  0.0000, // F9: Orderbook Depth
+         0.3208,  0.0234,  0.0000, // F10: Time Sine (Intraday)
+         -0.0541,  0.0000,  -0.1543, // F11: Last Close Price
     ];
     weights_data
 }
